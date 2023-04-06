9f5f5991d65f949486525e3edfea90217db480df
John <johnfarris217@gmail.com>
2023-04-05 20:36:43 -0500

    redo solution

--- javascript/fibonacci_recursive.js
+++ javascript/fibonacci_recursive.js
@@ -1,11 +1,21 @@
 function fibonacci(n) {
   // type your code here
+  if (n < 2) {
+    return n;
+  }
+
+  return fibonacci(n - 1) + fibonacci(n - 2)
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 0");
   console.log("=>", fibonacci(0));
+
+  console.log("");
+
+  console.log("Expecting: 1");
+  console.log("=>", fibonacci(1));
 
   console.log("");
 
